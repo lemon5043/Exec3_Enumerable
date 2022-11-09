921c20830acfe5a43360ebdb7ebe4ed742fca7d6
伯翰 侯 <john860703@gmail.com>
2022-11-09 14:11:24 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec3_Enumerable
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IEnumerable<int> countTo10 = Enumerable.Range(1, 10);
+			foreach (var item in countTo10)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IEnumerable<int> countTo15 = Enumerable.Range(10, 6);
+			foreach (var item in countTo15)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
